3f4df021062993e973d27dded2622a09c1e20bb7
Suhayb-Moh <suhaybmohamed@gmail.com>
2024-05-27 17:16:16 +0300

    Initial commit

---
+++ src/interceptors/serialize.interceptors.ts
@@ -0,0 +1,32 @@
+import {
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToInstance } from 'class-transformer';
+
+interface classConstructor {
+  new (...args: any[]): any;
+}
+
+export function Serialize(dto: classConstructor) {
+  return UseInterceptors(new ClassSerializerInterceptor(dto));
+}
+
+// @UseInterceptors(ClassSerializerInterceptor)
+export class ClassSerializerInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data: any) => {
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+          enableImplicitConversion: true,
+        });
+      }),
+    );
+  }
+}
